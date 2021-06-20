c19b64084b1151c5153544b363053c910220e5ce
AdriaanvanWyk <adriaanvanwyk93@gmail.com>
2021-06-20 23:17:25 +0100

    Added navigation styling with active links

--- src/mainContentPanelChange.js
+++ src/mainContentPanelChange.js
@@ -6,11 +6,26 @@ changePanel = targetPanelName => {
     if(CURRENT_PANEL_NAME !== targetPanelName) {
         const targetPanel = document.querySelector(`.${targetPanelName}`);
         targetPanel.classList.add('in-focus')
-    
+
         const currentPanel = document.querySelector(`.${CURRENT_PANEL_NAME}`);
         currentPanel.classList.remove('in-focus')
     
+        addBackrounddToActiveLink(targetPanelName)
+
         CURRENT_PANEL_NAME = targetPanelName;
     }
 
+    
+
+}
+
+addBackrounddToActiveLink = targetPanel => {
+    targetButtonClassName = targetPanel + "-button"
+    const targetButton = document.querySelector(`.${targetButtonClassName}`);
+    targetButton.classList.add('with-background-link')
+
+    currentButtonClassName = CURRENT_PANEL_NAME + "-button"
+    const currentButton = document.querySelector(`.${currentButtonClassName}`);
+    currentButton.classList.remove('with-background-link')
+    
 }
